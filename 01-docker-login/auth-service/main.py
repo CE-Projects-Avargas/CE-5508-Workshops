570da9d1b3668cab3dd0c4b0fcba780e93bb9cb8
JMLoria <joloria@estudiantec.cr>
2026-09-09 08:39:32 -0600

    Ignorar reporte de error de bibliotecas (temporal)

--- 01-docker-login/auth-service/main.py
+++ 01-docker-login/auth-service/main.py
@@ -1,10 +1,10 @@
 import os
 
-import mysql.connector
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-import bcrypt
+import mysql.connector # type: ignore
+from fastapi import FastAPI, HTTPException # type: ignore
+from fastapi.middleware.cors import CORSMiddleware # type: ignore
+from pydantic import BaseModel # type: ignore
+import bcrypt # type: ignore
 
 
 app = FastAPI(title="Auth Service")
